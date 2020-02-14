f83b8fb40ba08ff77889d16fd142b5513f754ca2
Misior <misiekgithub@gmail.com>
2020-02-14 21:53:25 +0100

    commit1

---
+++ test1.java
@@ -0,0 +1,33 @@
+class Test1 { // definicja klasy, PascalCase, klasy to rzeczowniki a, metdoy to czasowniki
+
+    public static int sum(int a, int b) { // definicja metody, camelCase, ona sumuje
+        int result = a + b;
+        return result;
+    }
+
+    public static int substract(int a, int b) { 
+        int result = a - b;
+        return result;
+    }
+
+    public static float divide(int a, int b) { 
+        float result = (float) a / b;
+        return result;
+    }
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+
+        int c = sum(a, b); // to jest to samo co int c = Test1.sum(a, b), ale w obrebie tej samej klasy nie musisz pisac nazwy klasy, jak wywolujesz metode statyczna
+        System.out.println(c);
+
+        int d = substract(a, b); // to jest to samo co int c = Test1.sum(a, b), ale w obrebie tej samej klasy nie musisz pisac nazwy klasy, jak wywolujesz metode statyczna
+        System.out.println(d);
+
+        float e = divide(a, b); // to jest to samo co int c = Test1.sum(a, b), ale w obrebie tej samej klasy nie musisz pisac nazwy klasy, jak wywolujesz metode statyczna
+        System.out.println(e);
+    }
+}
+
+// kompilowanie: otworz konsole (terminal), javac test1.java, java Test1
