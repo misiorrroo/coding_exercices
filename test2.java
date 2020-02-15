9d93d41ff4ee9c7982c46b4dd6997390b68aa49a
Misior <misiekgithub@gmail.com>
2020-02-15 14:39:58 +0100

    narazie max i min nie wiem co dale nie dziala:(

--- test2.java
+++ test2.java
@@ -1,9 +1,35 @@
 class Test2 {
-    
-    public static int findmin(int[] array) {
+
+    public static int findMin(int [] array) {
+    int numbers[]= {5484,231,4,64,613};
+    int nums[] = new int[5]; 
+    int min;
+    int max;
+    int avr;
+    {
+        min = nums[0];
+        for(int i=0; i < 10; i++) { 
+        if(numbers[i] < min) min = numbers[i];
+        int c = findMin(); 
+       }
+
+    public static int findMax(int[] array) {
+        max = nums[0];
+        for(int i=0; i < 10; i++) { 
+        if(numbers[i] > max) max = numbers[i];
+        int c = findMin(); 
+    }
+
+    public static float findAverage(int[] array) {
         return 0;
-    } // int[]
+
+    }
 
     public static void main(String args[]) {
+        int c = findMin(min);
+        // one mają conajmniej jeden element
+        // testy
+        System.out.println("low value" + min);
     }
+
 }
