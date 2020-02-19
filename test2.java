60de2c37bf2ed44d8682df481e5dacbd224a9d60
Fibonacci sequence <60035675+misiorrroo@users.noreply.github.com>
2020-02-19 09:34:21 +0100

    Update test2.java

--- test2.java
+++ test2.java
@@ -28,12 +28,12 @@ class Test2 {
     }
 
     public static void main(String args[]) {
-
+	int tab[] = {4,6,87, 99, 23}
         // one mają conajmniej jeden element
         // testy
-        int max = findMax();
-        int min = findMin();
-        double avr = findAvr();
+        int max = findMax(tab[]);
+        int min = findMin(tab[]);
+        double avr = findAvr(tab[]);
         System.out.println("max value" + max);
         System.out.println("min value" + min);
         System.out.println("avaerage value" + avr);
