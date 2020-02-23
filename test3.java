0275b272f4378ea253db7b4ec66a19efa73be684
Misior <misiekgithub@gmail.com>
2020-02-23 17:47:10 +0100

    działa!!!

--- test3.java
+++ test3.java
@@ -1,8 +1,8 @@
 //W test3.java zrób funkcję public static int findSecondMax(int[] array)
-class Test2 {
+class test3 {
     public static int findSecondMax(int[] array) {
         
-        int max;
+        int max = array[0];
         int secondmax = array[0];
         for (int i = 0; i < array.length; i++) {          
             if (array[i] > max) {
@@ -12,14 +12,12 @@ class Test2 {
 
             secondmax = array[i];
         }
-    }
             return secondmax;             //cos jest tu zjebane wiem ze mialem nie wrzucac nie kompilujacego sie kodu ale nie wiem co tu jest nie OK.
-
+    }
     public static void main(String args[]) {
-        int[] tab = {4, 6, 87, 99, 23};
-        int max = findSecondMax(tab);
-        int secondmax;
+        int[] tab = {-5, -7, -10, -3, -4};
+        int secondmax = findSecondMax(tab);
         System.out.println("second max value " + secondmax);
     }
-
 }
+
