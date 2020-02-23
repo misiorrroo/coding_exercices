bc477f52b2db662d486d269890bc2f7045b692cc
Misior <misiekgithub@gmail.com>
2020-02-24 00:17:41 +0100

    wszystko działa

--- test4.java
+++ test4.java
@@ -1,21 +1,30 @@
 class Test4 {
 
-    public static int findMaxSum(int[] array) {
+    public static int findMin(int[] array) {
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
             }
         }
-        return maxsum;
+        return min;
     }
 
+    public static int findMaxSum(int[] array) {
+        int sum = 0; 
+        int i;
+        for (i = 0; i < array.length; i++)
+        sum += array[i];
+        return sum;
+        }
 
-    public static void main(String args[]) { //cos jest nie tak, pisze z palca jbc
-        int[] tab = {-5, -7, -10, -3, -4};
-        // one mają conajmniej jeden element
-        // testy
-        int maxsum = findMaxSum(tab);
-        System.out.println("Max sum from 4 elements" + maxsum);
+    public static void main(String args[]) { 
+        int[] tab = {5 ,4 ,3 ,2 ,1};
+        int min = findMin(tab);
+        int sum = findMaxSum(tab);
+        int summin = sum - min;
+        System.out.println("min value " + min);
+        System.out.println("sum value is " + sum);
+        System.out.println("Max sum from 4 elements " + summin);
     }
-
+}
