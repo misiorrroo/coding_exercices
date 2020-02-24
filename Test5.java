fcf932cad078fc9e6168808d0e91514540c4ddf8
Misior <misiekgithub@gmail.com>
2020-02-24 22:27:33 +0100

    jeszcze nie gotowe

---
+++ Test5.java
@@ -0,0 +1,23 @@
+class Test5 {
+    boolean isLeap;
+    int year;
+
+    public static boolean isLeap(int year) {
+        year = 0;
+        for (year = 0; year < 2020; year++)
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0)
+                        isLeap = true;
+                }
+            }
+        return isLeap;
+    }
+
+    public static void main(String args[]) {
+
+        boolean c = leapYear(year);
+        System.out.println(c);
+
+    }
+}
