ff959e66f58563ca1f9f8cca9bffe16655259ac8
Fibonacci sequence <60035675+misiorrroo@users.noreply.github.com>
2020-03-03 10:47:07 +0100

    Update Test6.java
    k

--- Test6.java
+++ Test6.java
@@ -1,21 +1,29 @@
-class Test6 { 
-    public static boolean caller(int n) { 
-    if ((n == 2) && (n==4)){
-      return true;
-    } 
+public class Test6 { 
+    public static boolean caller(int n) {
+    if(n=1){
+    return false;
+    }
+    if ((n > 2 && n < 5) && (n%2==0)){
+    return true;
+    }else{
+    return false;
+    }
     if((n >= 6) && (n <= 20) && (n % 2 == 0)){
     return true;
+    }else{
+    return false;
     }
     if ((n > 20) && (n % 2 == 0)){
     return true;
     } 
-    else 
+    else {
     return false;
+    }
     }
 
     public static void main(String args[]) {
         int n = 4;
-        boolean c = caller(n);
+        boolean c = caller;
         if(c == true){
         System.out.println("Weird");
         }
