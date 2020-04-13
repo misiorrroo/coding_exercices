bb9e72a5971eec0446f07a8ba094bf47bbec9f27
Misior <misiekgithub@gmail.com>
2020-04-13 21:18:18 +0200

    zostala konkatynacja

--- Test13.java
+++ Test13.java
@@ -1,23 +1,18 @@
 public class Test13 {
     public static boolean haveCommonElement(String[] first, String[] second) {
-        if (first.length == second.length) {
-            for (int i = 0; i < first.length; i++) {
 
-                if (first[i] == second[i] || first[i] == second[i + 1] || first[i] == second[i + 2]
-                        || first[i+1] == second[i] || first[i+1] == second[i + 1] || first[i+1] == second[i + 2]
-                        || first[i+2] == second[i] || first[i+2] == second[i + 1] || first[i+2] == second[i + 2]) {
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) 
+                if (first[i] == second[j]) {
                     return true;
                 }
-            }
-        return false;
-        }else{
-        return false;
         }
+        return false;
     }
 
     public static void main(String args[]) {
         String[] first = { "Aniela", "mnie", "wkurwia" };
-        String[] second = { "Anielaa", "jest", "super" };
+        String[] second = { "Aniela", "jest", "super" };
         if (haveCommonElement(first, second)) {
             System.out.println("Conajmniej jeden element jest wspólny");
         } else {
--- Test14.java
+++ Test14.java
@@ -1,23 +1,22 @@
 public class Test14 {
-    public static boolean intChecker(int[] array, int[] array2) {
+    public static boolean equalTwoArray(int[] array, int[] array2) {
         if (array.length == array2.length) {
-            for (int i = 0; i < array.length; i++)
-
-            if (array[i] == array2[i] && array[i] == array2[i + 1] && array[i] == array2[i + 2]
-            && array[i+1] == array2[i] && array[i+1] == array2[i + 1] && array[i+1] == array2[i + 2]
-            && array[i+2] == array2[i] && array[i+2] == array2[i + 1] && array[i+2] == array2[i + 2]);
+            for (int i = 0; i < array.length; i++) {
+                  if (array[i] != array2[i]) {
+                      return false;
+                  }
+            }
             return true;
-       }else{
-        return false;
-       }
+        } else {
+            return false;
+        }
     }
-    
 
     public static void main(String args[]) {
-        int[] tab = { 1, 2, 3 };
-        int[] tab2 = { 1, 2, 3 };
-        boolean intChecker = intChecker(tab, tab2);
-        if (intChecker) {
+        int[] tab = { 1, 2, 3, 4 };
+        int[] tab2 = { 1, 2, 3, 4, 5 };
+        boolean equalTwoArray = equalTwoArray(tab, tab2);
+        if (equalTwoArray) {
             System.out.println("Te same tablice");
         } else {
             System.out.println("Nie te same tablice");
