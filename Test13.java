1044e22c7c03a935a15a1d64fd483a149a510d82
Misior <misiekgithub@gmail.com>
2020-04-13 19:53:22 +0200

    wyjatki

--- Test13.java
+++ Test13.java
@@ -1,15 +1,23 @@
 public class Test13 {
-public static boolean haveCommonElement(String[] first, String[] second) {
-    for (int i = 0; i < first.length; i++){
-        if(first[0]==second[0]||first[0]==second[1]||first[0]==second[2]||first[1]==second[0]||first[1]==second[1]||first[1]==second[2]||first[2]==second[0]||first[2]==second[1]||first[2]==second[2]&&first.length==second.length){
-            return true;
+    public static boolean haveCommonElement(String[] first, String[] second) {
+        if (first.length == second.length) {
+            for (int i = 0; i < first.length; i++) {
+
+                if (first[i] == second[i] || first[i] == second[i + 1] || first[i] == second[i + 2]
+                        || first[i+1] == second[i] || first[i+1] == second[i + 1] || first[i+1] == second[i + 2]
+                        || first[i+2] == second[i] || first[i+2] == second[i + 1] || first[i+2] == second[i + 2]) {
+                    return true;
+                }
+            }
+        return false;
+        }else{
+        return false;
         }
     }
-        return false;
-}
+
     public static void main(String args[]) {
-        String[] first = { "Skrzyniaa", "mieszka", "pakamera" };
-        String[] second = { "Skrzynia", "w", "pakamerze" };
+        String[] first = { "Aniela", "mnie", "wkurwia" };
+        String[] second = { "Anielaa", "jest", "super" };
         if (haveCommonElement(first, second)) {
             System.out.println("Conajmniej jeden element jest wspólny");
         } else {
--- Test14.java
+++ Test14.java
@@ -1,10 +1,17 @@
 public class Test14 {
     public static boolean intChecker(int[] array, int[] array2) {
-        if (array[0] == array2[0] && array[1] == array2[1] && array[2] == array2[2] && array.length == array2.length) {
+        if (array.length == array2.length) {
+            for (int i = 0; i < array.length; i++)
+
+            if (array[i] == array2[i] && array[i] == array2[i + 1] && array[i] == array2[i + 2]
+            && array[i+1] == array2[i] && array[i+1] == array2[i + 1] && array[i+1] == array2[i + 2]
+            && array[i+2] == array2[i] && array[i+2] == array2[i + 1] && array[i+2] == array2[i + 2]);
             return true;
-        }
+       }else{
         return false;
+       }
     }
+    
 
     public static void main(String args[]) {
         int[] tab = { 1, 2, 3 };
