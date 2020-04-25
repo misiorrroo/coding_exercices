b7e5c3aaa03a083aed5b334dae9a35717b2f70c8
Misior <misiekgithub@gmail.com>
2020-04-25 13:41:17 +0200

    drzewo też

--- Test16.java
+++ Test16.java
@@ -2,30 +2,19 @@
 public class Test16 {
 
     public static int multiplicationWithoutOperator(int a, int b) {
-        if (a == 0) {
-            return 0;
-        }
-        if (b == 0) {
-            return 0;
-        }
-        if (a == 1) {
-            return b;
-        }
-        if (b == 1) {
-            return a;
-        }else{
+
             int result = 0;
-        for(int i =0 ;i<b; i++){
-            result = result + a;
+            for (int i = 0; i < b; i++) {
+                result = result + a;
+            }
+            return result;
         }
-        return result;
-        }
-    }
+    
 
     public static void main(String args[]) {
         int a = 5;
         int b = 6;
-        int multiplication = multiplicationWithoutOperator(a,b);
+        int multiplication = multiplicationWithoutOperator(a, b);
         System.out.println("Wynik to:" + multiplication);
     }
 }
