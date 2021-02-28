30721902f0b68311d473b0dc0ff150a3f5a9262d
Misior <misiekgithub@gmail.com>
2021-02-28 19:18:26 +0100

    molla

---
+++ cos.java
@@ -0,0 +1,9 @@
+public class cos {
+    public static void cos( array []){
+        int scosiowany [] = new int ;
+    }
+    public static void main(String[] args) {
+    int [] tab = {9,6 ,523456 ,32456,234, 6,235,23456,2346,24,6,246,2464,2246,26,246,2,6,246,24};  
+        
+    }
+}
